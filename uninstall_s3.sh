eca8562de80cc5a161c83ec60d32ef7777f9c35b
victor.salaun <victor.salaun@gmail.com>
2017-09-21 23:37:18 +0200

    add specific install and uninstall scripts for s3

---
+++ uninstall_s3.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+TEMPLATE_FILE_NAME='mugshot-s3.cfn.yml'
+PACKAGE_FILE_NAME='mugshot-s3-xfm.cfn.yml'
+STACK_NAME='mugshot-s3'
+BUCKET_NAME_TEMPLATE="mug-shot-template-s3"
+
+
+# Check if the aws cli is installed
+if ! command -v aws > /dev/null; then
+    echo "aws cli was not found. Please install before running this script."
+    exit 1
+fi
+
+# Try to remove the package
+if aws cloudformation delete-stack --stack-name ${STACK_NAME}; then
+    echo "Package ${STACK_NAME} removed successfully"
+else
+    echo "Failed removing package ${STACK_NAME}"
+fi
